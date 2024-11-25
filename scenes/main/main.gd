1754402baa3f3ed7882d0edaf29d96060d61fcc5
Nathan Leiby <nathanleiby@gmail.com>
2024-11-25 12:01:12 -0800

    working on tilemap collisions

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -12,18 +12,18 @@ func _process(delta: float) -> void:
 	pass
 
 func new_game() -> void:
-	$Player.start(Vector2(0,0))
+	$Player.start(Vector2(64, 64))
 	var spawn_timer := Timer.new()
 	spawn_timer.wait_time = 3.0
 	add_child(spawn_timer)
 	spawn_timer.start()
 	spawn_timer.connect("timeout", _on_spawn_timer_timeout)
-	
-	
+
+
 func _on_spawn_timer_timeout() -> void:
 	print("spawn timer timeout")
 	var new_enemy = enemy_scene.instantiate()
 	await new_enemy.is_node_ready()
-	new_enemy.start_position(Vector2(randf_range(50,150), randf_range(50,150)))
-	new_enemy.start_velocity(Vector2(150,150))
+	new_enemy.start_position(Vector2(randf_range(50, 150), randf_range(50, 150)))
+	new_enemy.start_velocity(Vector2(150, 150))
 	add_child(new_enemy)
