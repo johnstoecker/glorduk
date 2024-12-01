4c6055dd15ddc47d60a9d9d544d3c1bee6be49da
Nathan Leiby <nathanleiby@gmail.com>
2024-11-30 17:50:36 -0800

    move some enemy config into @export

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,69 +1,57 @@
 extends RigidBody2D
 class_name Enemy
-# z-index 5
-# collision layer 3
-# mask 2 + 3
-# lock rotation: true
-# gravity scale = 0
 
 @onready var _animated_sprite = $AnimatedSprite2D
-
 
 @export var enemy_scene: PackedScene
 
 enum States {IDLE, RUNNING, ATTACKING, DEAD}
-
 var current_state = States.IDLE
 
-var speed = 100
-
+@export var movement_speed = 100
 var current_path = []
 var current_path_index = 0
+var enemy_path_calc_timer = 0
+var recalculate_enemy_path_rate = 0.5 # recalculate paths every x seconds
 
 var attack_timer = 0
-var attack_proximity = 50 # pixels (50 is melee range)
-var attack_rate = 0.5 # seconds-per-attack (lower number is a faster)
+@export var attack_proximity = 50 # pixels (50 is melee range)
+@export var attack_rate = 0.5 # attack every x seconds (lower number is a faster)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_animated_sprite.play("e_walk")
 
-var enemy_path_calc_timer = 0
-var recalculate_enemy_paths_at = 0.5 # recalculate paths every x seconds
-
-func choose_target() -> Player:
-	var players = get_tree().get_nodes_in_group(Globals.GROUP_PLAYER)
-	if !len(players):
-		return
-	var player: Player = players[0]
-	return player
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if current_state == States.DEAD:
 		return
 
+	# update timers
+	attack_timer += delta
+	enemy_path_calc_timer += delta
+
 	# choose target
 	var player = choose_target()
 	if not player:
 		return
 
-	attack_timer += delta
-
+	# Decide what to do...
 	if player.global_position.distance_to(position) <= attack_proximity:
+		# is it time to attack?
+		if attack_timer < attack_rate:
+			return
+		attack_timer = 0
+
 		# attack
-
-		if attack_timer >= attack_rate:
-			attack_timer = 0
-			current_state = States.ATTACKING
-			linear_velocity = Vector2(0, 0)
-			attack(player)
-
-		enemy_path_calc_timer = 0 # this ensures we'll repath the next time the enemy moves
+		current_state = States.ATTACKING
+		linear_velocity = Vector2(0, 0)
+		attack(player)
 	else:
-		# is it time to act (repath, attack, etc)?
-		enemy_path_calc_timer += delta
-		if enemy_path_calc_timer < recalculate_enemy_paths_at:
+		# is it time to repath movement?
+		if enemy_path_calc_timer < recalculate_enemy_path_rate:
 			return
 		enemy_path_calc_timer = 0
 
@@ -72,23 +60,31 @@ func _process(delta: float) -> void:
 		current_path = path
 		current_path_index = 1
 
-		# move
+		# change movement velocity
 		_animated_sprite.play("e_walk")
 		current_state = States.RUNNING
 		if current_path.size() > 2:
-			linear_velocity = (current_path[current_path_index] - position).normalized() * speed
+			linear_velocity = (current_path[current_path_index] - position).normalized() * movement_speed
 		else:
-			linear_velocity = (player.global_position - position).normalized() * speed
+			linear_velocity = (player.global_position - position).normalized() * movement_speed
+
 
 func init(pos: Vector2, direction: Vector2):
 	position = pos
-	linear_velocity = direction.normalized() * speed
+	linear_velocity = direction.normalized() * movement_speed
 
-func _on_body_entered(body):
-	pass
+
+func choose_target() -> Player:
+	var players = get_tree().get_nodes_in_group(Globals.GROUP_PLAYER)
+	if !len(players):
+		return
+	var player: Player = players[0]
+	return player
+
 
 func attack(_player: Player):
 	assert(false, "Enemy.attack() is a virtual method. It must be implemented in the Enemy subclass.")
+
 
 func die():
 	_animated_sprite.play("die")
--- enemies/troll/troll.gd
+++ enemies/troll/troll.gd
@@ -2,12 +2,6 @@ extends Enemy
 
 var arrow_scene = preload("res://entities/projectiles/arrow/arrow.tscn")
 
-## Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	super()
-	attack_proximity = 200
-	attack_rate = 1.0
-
 func attack(player: Player):
 	var arrow = arrow_scene.instantiate()
 	var direction = player.global_position - position
